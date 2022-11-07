c2a98221de228fe8ba166af38c75bb0eb93c147a
igh197 <igh197@daum.net>
2022-11-07 12:36:26 +0900

    developing default environment

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from tkinter.ttk import Entry
+
+from tkinter import *
 import sklearn as sk
 import matplotlib as mat
+import simpy as sim
+import numpy as np
 
+
+def simulation_community(whole_size, number_of_friendly, number_of_nice, number_of_bad):
+    root = Tk()
+    whole_size = Entry(root,)
