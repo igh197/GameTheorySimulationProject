bf7a995597274d6b0f2dd24f8e439b125a0e34ea
igh197 <igh197@daum.net>
2022-11-10 12:10:22 +0900

    simulation community revision

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from tkinter.ttk import Entry
 
-from tkinter import *
-import sklearn as sk
+import tkinter as tki
 import matplotlib as mat
 import simpy as sim
 import numpy as np
 
+window = tki.Tk()
+window.title = "GameTheorySimulation"
+window.resizable(True, True)
+
 
 def simulation_community(whole_size, number_of_friendly, number_of_nice, number_of_bad):
-    root = Tk()
-    whole_size = Entry(root,)
+    whole_size = tki.Entry(window, width=30)
+    number_of_friendly = tki.Entry(window, width=30)
+    number_of_nice = tki.Entry(window, width=30)
+    number_of_bad = tki.Entry(window, width=30)
